7f10df5a26efcf692325b77a364f154b81062a52
hyyc116 <hyyc116@gmail.com>
2017-03-05 11:09:00 -0500

    add stat sc headers

--- tools/sc_xml_statistics.py
+++ tools/sc_xml_statistics.py
@@ -6,6 +6,7 @@ from bs4 import SoupStrainer
 from bs4 import BeautifulSoup as bs
 import re
 from collections import defaultdict
+from datetime import datetime
 
 
 
@@ -27,6 +28,7 @@ def parse_all(indexpath):
     headerdict=defaultdict(int)
     doiset=set()
     paths=[]
+    sys.stderr.write("time:{:}".format(datetime.now()))
     for line in open(indexpath):
         line=line.strip()
         doi,headers = parse_xml(line)
@@ -37,7 +39,7 @@ def parse_all(indexpath):
             doiset.add(doi)
             paths.append(line)
             progress_count+=1
-            if progress_count%1000==1:
+            if progress_count%10==1:
                 sys.stderr.write("progress:{:}\n".format(progress_count))
         
         for header in headers:
@@ -49,11 +51,13 @@ def parse_all(indexpath):
     for k,v in sorted(headerdict.items(),key=lambda x:x[1],reverse=True):
         if v>100:
             high_frequency_count+=1
-            print "{:}\t{:}".fromat(k,v)
+            print "{:}\t{:}".format(k,v)
 
     sys.stderr.write("Unique papers:{:}.\nNumber of headers:{:}.\n".format(progress_count,header_count))
     sys.stderr.write("Ratio of high frequency headers: {:.10f}\n".format(high_frequency_count/float(header_count)))
     open("data/paths.txt","w").write("\n".join(paths))
+    sys.stderr.write("time:{:}".format(datetime.now()))
+
 
 if __name__=="__main__":
     parse_all(sys.argv[1])
